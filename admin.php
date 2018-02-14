bf6b49577e20f21af1042fe25bac6d5a2cf4bd7b
Евлахина Анна <35760192+EvaFlareon@users.noreply.github.com>
2018-02-14 08:33:25 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -3,7 +3,7 @@
 $uploadfile = "files/";
 
 if (isset($_FILES['userfile']['name']) && !empty($_FILES['userfile']['name'])) {
-	if ($_FILES['userfile']['error'] == UPLOAD_ERR_OK && move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile.date(YmdHis).$_FILES['userfile']['name'])) {
+	if ($_FILES['userfile']['error'] == UPLOAD_ERR_OK && move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile.date('YmdHis').$_FILES['userfile']['name'])) {
 		echo "<p style='color:green'>Файл с тестами загружен</p>";
 	} else {
 		echo "<p style='color:red'>Ошибка: Файл с тестами не загружен</p>";
